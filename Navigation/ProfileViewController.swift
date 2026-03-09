295ff3806404466c1ff324b3afa19114308facaf
Artem Smirnov <cayman058@gmail.com>
2026-03-09 16:26:09 +0300

    Вёрстка экранов

--- Navigation/ProfileViewController.swift
+++ Navigation/ProfileViewController.swift
@@ -1,18 +1,49 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
     private let profileHeaderView = ProfileHeaderView()
-
+    
+    private let bottomButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Bottom Button", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 14
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .lightGray
-        view.addSubview(profileHeaderView)
+        title = "Profile"
+        
+        setupProfileHeader()
+        setupBottomButton()
     }
-
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        profileHeaderView.frame = view.bounds
+    
+    private func setupProfileHeader() {
+        profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(profileHeaderView)
+        
+        NSLayoutConstraint.activate([
+            profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            profileHeaderView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            profileHeaderView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            profileHeaderView.heightAnchor.constraint(equalToConstant: 220)
+        ])
+    }
+    
+    private func setupBottomButton() {
+        view.addSubview(bottomButton)
+        
+        NSLayoutConstraint.activate([
+            bottomButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            bottomButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            bottomButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bottomButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
 }
