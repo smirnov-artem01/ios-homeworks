44041e95a7cd216c9ccd0b17d62b161098c685d4
Artem Smirnov <cayman058@gmail.com>
2026-04-23 00:00:06 +0300

    Новое ДЗ (на основе фото + галерея)

--- StorageService/Post.swift
+++ StorageService/Post.swift
@@ -1,14 +1,22 @@
 import Foundation
 
-struct Post {
-    let author: String
-    let description: String
-    let image: String
-    let likes: Int
-    let views: Int
+public struct Post {
+    public let author: String
+    public let description: String
+    public let image: String
+    public let likes: Int
+    public let views: Int
+    
+    public init(author: String, description: String, image: String, likes: Int, views: Int) {
+        self.author = author
+        self.description = description
+        self.image = image
+        self.likes = likes
+        self.views = views
+    }
 }
 
-extension Post {
+public extension Post {
     static func makePosts() -> [Post] {
         return [
             Post(
